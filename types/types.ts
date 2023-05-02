f919c98d21405d75324b5d654268a8eeaf33ad6d
Gregg Fine <gregg@greggfinedev.com>
2023-05-02 13:56:41 -0400

    Updates

---
+++ types/types.ts
@@ -0,0 +1,33 @@
+export type Food = {
+  x: number;
+  y: number;
+  color?: any;
+  noteName?: any;
+  noteFrequency?: any;
+};
+
+export type Position = {
+  x: number;
+  y: number;
+};
+
+export type Note = {
+  note: string;
+  frequency: number;
+};
+
+// export type Tonics = {
+//   [key: string]: number;
+//   C: number;
+//   "C#": number;
+//   D: number;
+//   "D#": number;
+//   E: number;
+//   F: number;
+//   "F#": number;
+//   G: number;
+//   "G#": number;
+//   A: number;
+//   "A#": number;
+//   B: number;
+// };
